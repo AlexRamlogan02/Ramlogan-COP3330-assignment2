919e3477ee39e90fb6549288c58d38b38ee28ff6
AlexRamlogan02 <AlexRamlogan02@knights.ucf.edu>
2021-09-27 23:23:50 -0400

    fixed comments

--- src/main/java/ex40/App.java
+++ src/main/java/ex40/App.java
@@ -1,8 +1,19 @@
 package ex40;
 
+import java.util.Scanner;
+
 /*
  *  UCF COP3330 Fall 2021 Assignment 2 Solution
  *  Copyright 2021 Alexandra Ramlogan
  */
 public class App {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String search;
+
+        System.out.print("Enter a search string: ");
+        search = scan.nextLine();
+
+        //unable to add more
+    }
 }
